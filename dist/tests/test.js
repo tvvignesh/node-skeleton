194c4f4404c45e6e58c5ed471fbd87811bb7d391
Vignesh T.V <vignesh@timecampus.com>
2019-09-11 21:48:35 +0530

    Upgraded all npm packages to their latest versions Fixed eslint issues to use the new plugins rather than the old one Added lint rule to nodemon on every execution

--- dist/tests/test.js
+++ dist/tests/test.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -12,7 +13,7 @@ const ava_1 = require("ava");
 ava_1.default('foo', t => {
     t.pass();
 });
-ava_1.default('bar', (t) => __awaiter(this, void 0, void 0, function* () {
+ava_1.default('bar', (t) => __awaiter(void 0, void 0, void 0, function* () {
     const bar = Promise.resolve('bar');
     t.is(yield bar, 'bar');
 }));
